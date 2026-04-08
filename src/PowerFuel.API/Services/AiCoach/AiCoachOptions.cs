62cc38e8e7d01d178e01cb92bd6cc38291f6f29e
moustafa <moustafamohamedelgazar987@gmail.com>
2026-04-08 15:32:50 +0200

    feat: integrate FastAPI AI coach into .NET API

---
+++ src/PowerFuel.API/Services/AiCoach/AiCoachOptions.cs
@@ -0,0 +1,9 @@
+namespace PowerFuel.API.Services.AiCoach;
+
+public sealed class AiCoachOptions
+{
+    public const string SectionName = "AiCoach";
+
+    public string BaseUrl { get; init; } = "http://localhost:8000";
+}
+
